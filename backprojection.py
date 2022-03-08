d2b11242548ed4a1e3dd7c1a7938b1aa732ed389
bommaritom <bommaritom@carleton.edu>
2022-03-08 11:02:06 -0500

    clean up code

--- backprojection.py
+++ backprojection.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from numpy import arange
+from numpy import arange, matrix
 import io
 from pylab import meshgrid,cm,imshow,contour,clabel,colorbar,axis,title,show
 from mpl_toolkits.mplot3d import Axes3D
@@ -9,29 +9,27 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 import skimage as sk
-from skimage.data import shepp_logan_phantom
-from skimage.data import checkerboard
-from skimage.data import camera
+from skimage.data import shepp_logan_phantom, checkerboard, camera
 from skimage.transform import radon, rescale
-from numpy import matrix
 
-
-
-# create shepp logan phantom image
+# select grayscale image
 
 image = shepp_logan_phantom()
+
+# mask the image so that it is a circle (code from skimage)
+
 l_x, l_y = image.shape[0], image.shape[1]
 X, Y = np.ogrid[:l_x, :l_y]
 outer_disk_mask = (X - l_x / 2)**2 + (Y - l_y / 2)**2 > (l_x / 2)**2 - 1
 image[outer_disk_mask] = 0
 
+# rescale image
 
 image = rescale(image, scale=.4, mode='reflect')
 
-
 # create radon transform
 
-NUM_ANGLES = 1000
+NUM_ANGLES = 100
 angles = np.linspace(0., 180., NUM_ANGLES, endpoint=False)
 sinogram = radon(image, theta=angles)
 
@@ -45,7 +43,7 @@ plt.imshow(sinogram, cmap=plt.cm.Greys_r,
            aspect='auto')
 plt.show()
 
-# initialize some constants
+# initialize constants
 
 N = np.shape(sinogram)[0]
 
@@ -57,14 +55,13 @@ P = np.transpose(sinogram)
 
 S = np.fft.fft(P)
 
-# filter the slices
-
 # filter based on https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4341983/
 
-ramp = .5*(np.sinc(np.linspace(-N//2,N//2,N))) - .25*np.square(np.sinc(np.linspace(-N//2, N//2, N)/2))
-filter = np.fft.fft(np.fft.fftshift(ramp))
+x_vals = np.linspace(-N//2,N//2,N)
+ramp = .5 * (np.sinc(x_vals)) - .25 * np.square(np.sinc(x_vals/2))
+RAMP = np.fft.fft(np.fft.fftshift(ramp))
 
-S_filtered = S * filter
+S_filtered = S * RAMP
 
 # inverse fourier transform of each slice
 
@@ -74,23 +71,36 @@ Q = np.fft.ifft(S_filtered)
 
 f = np.zeros((N,N))
 
+# this could be rewritten without the loop, but it would be hard!!!
 for k in range(len(Q)):
+
 	theta = k/NUM_ANGLES * np.pi
 
-	p1 = np.arange(N)[::-1,None]
-	p2 = np.arange(N)[None,:]
+	# get parameterization
 
-	vals = (p2 - (N/2))*np.cos(theta) + (p1 - N/2)*np.sin(theta) + N/2
-	floors = np.minimum(np.floor(vals).astype(int), len(Q[k])-1)
-	ceils = np.minimum(np.ceil(vals).astype(int), len(Q[k])-1)
-	
-	f += np.where(np.logical_and(vals >= 0, vals < len(Q[k])), np.take(Q[k],floors).real + (vals-floors)*(np.take(Q[k],ceils)-np.take(Q[k],floors)).real, np.zeros(np.shape(f)))
+	row_ind = np.arange(N)[::-1,None]
+	col_ind = np.arange(N)
+
+	t = (col_ind - (N/2))*np.cos(theta) + (row_ind - N/2)*np.sin(theta) + N/2
+
+	# perform linear interpolation
+
+	fl = np.minimum(np.floor(t).astype(int), len(Q[k])-1)
+	ce = np.minimum(np.ceil(t).astype(int), len(Q[k])-1)
+	up = np.take(Q[k],ce).real
+	lo = np.take(Q[k],fl).real
+
+	interp = lo + (t-fl)*(up-lo)
+
+	# add backprojection to the reconstructed image
+
+	in_bounds = np.logical_and(t >= 0, t < len(Q[k]))
+
+	f += np.where(in_bounds, interp, np.zeros(np.shape(f)))
 
 	print("  " + str(np.round(100*(k+1)/NUM_ANGLES, 1))+ "% complete", end='\r')
 
-
 f *= np.pi/NUM_ANGLES
-
 
 # display everything
 
@@ -104,23 +114,17 @@ fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(8, 4.5),
 min = 0
 max = 1
                      
-                     
 ax1.set_title("Original")
-ax1.imshow(image, cmap=plt.cm.Greys_r, vmin = min, vmax = max)
-#ax1.imshow(image, cmap=plt.cm.Greys_r)
-
+im1 = ax1.imshow(image, cmap=plt.cm.Greys_r, vmin = min, vmax = max)
 
 ax2.set_title("Reconstruction\nFiltered backprojection\n"+str(NUM_ANGLES)+" angles")
 im2 = ax2.imshow(f, cmap=plt.cm.Greys_r, vmin = min, vmax = max)
-#im2 = ax2.imshow(f, cmap=plt.cm.Greys_r)
 
-
-ax3.set_title("1 minus absolute\nreconstruction error\nFiltered back projection")
-#im3 = ax3.imshow(f - image, cmap=plt.cm.Greys_r, **imkwargs)
-ax3.imshow(max-np.abs(f-image), cmap = plt.cm.Greys_r, vmin = min, vmax = max)
+ax3.set_title("Absolute\nreconstruction error\nFiltered back projection")
+im3 = ax3.imshow(np.abs(f-image), cmap = plt.cm.Greys_r, vmin = min, vmax = max)
 
 fig.subplots_adjust(right=0.85)
 
 cbar_ax = fig.add_axes([0.88, 0.15, 0.04, 0.7])
-fig.colorbar(im2, cax = cbar_ax)
+fig.colorbar(im1, cax = cbar_ax)
 plt.show()
